cc438b8e6b8eae05571053a90e4619ae6f6ca9d4
Ronald Kuip <Ronald.Kuip@live.nl>
2022-07-28 13:31:12 +0200

    Wijziging over externe wijziging heen 9en wer erop)

--- Pipo.java
+++ Pipo.java
@@ -6,3 +6,5 @@ We zijn nu weer terug bij de file zoals die bij de repo stond, toch?
 
 No gm eer tekst pom naar github te brengen.
   NU wijzig ik een externe bron en wil ik dat erug fetchen.
+
+En weer erop
